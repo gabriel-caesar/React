febd1791d0b017a8808734dcb9d142fc243cd637
gabriel-caesar <gabriel.mdonno@hotmail.com>
2025-03-09 23:08:40 -0400

    changes to fix deployment issues

--- memory-card-project/src/main.jsx
+++ memory-card-project/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './components/App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter as Router } from 'react-router-dom';  // Import HashRouter
+import './index.css';
+import App from './components/App';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>  {/* Wrap your App with HashRouter */}
+      <App />
+    </Router>
   </StrictMode>,
-)
+);
