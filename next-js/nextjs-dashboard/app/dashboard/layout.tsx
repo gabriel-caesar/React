ef510125752d2f8e7ba238f60143ef7c81aa1971
gabriel-caesar <gabriel.mdonno@hotmail.com>
2025-07-30 01:16:56 -0400

    last commit

--- next-js/nextjs-dashboard/app/dashboard/layout.tsx
+++ next-js/nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,6 +1,12 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Home',
+};
  
 export default function Layout({ children }: { children: React.ReactNode }) {
+  
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
