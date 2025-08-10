6d285722b2dfb0574f2e2b10464457e3fa5f5c78
gabriel-caesar <gabriel.mdonno@hotmail.com>
2025-08-10 18:19:39 -0400

    [Features/Fix] Added Friends and Plans routes - Added routes don't have relevant content yet - Fixed last commit by running npm run build

---
+++ next-js/gym-app/app/dashboard/friends/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <div className='flex justify-center items-center h-screen w-screen text-3xl'>
+      <h1 className='rounded bg-red-400 p-2'>
+        Hi From Friends!
+      </h1>
+    </div>
+  )
+}
--- next-js/gym-app/app/ui/dashboard/sidebar-nav.tsx
+++ next-js/gym-app/app/ui/dashboard/sidebar-nav.tsx
@@ -1,30 +1,71 @@
-import { Power } from 'lucide-react';
+'use client'
+
+import { ClipboardCheckIcon, UsersRound, House, Power } from 'lucide-react';
 import { signUserOut } from '../../actions/auth';
 import { AnimatePresence, motion } from 'motion/react';
+import Link from 'next/link';
 
 export default function SideBarNav({ openSideBar }: { openSideBar: boolean }) {
+
+  const MotionLink = motion.create(Link);
+
   return (
     <nav
       id='sidebar'
-      className={`h-screen bg-neutral-600 ${openSideBar ? 'w-100' : 'w-0'} relative transition-all`}
+      className={`h-screen bg-neutral-600 ${openSideBar ? 'w-100' : 'w-0'} flex flex-col justify-start items-center relative transition-all`}
     >
-      <form
+      <MotionLink 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+        href='/dashboard'
+        className={`${openSideBar ? 'flex' : 'hidden'} justify-between items-center mt-2 p-2 w-11/12 rounded-md transition-all duration-150 hover:cursor-pointer hover:bg-neutral-800`}
+      >
+        Dashboard
+        <House strokeWidth={1.5} />
+      </MotionLink>
+
+      <MotionLink 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
+        href='/dashboard/plans'
+        className={`${openSideBar ? 'flex' : 'hidden'} justify-between items-center mt-2 p-2 w-11/12 rounded-md transition-all duration-150 hover:cursor-pointer hover:bg-neutral-800`}
+      >
+        Plans
+        <ClipboardCheckIcon strokeWidth={1.5} />
+      </MotionLink>
+
+      <MotionLink 
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        href='/dashboard/friends'
+        className={`${openSideBar ? 'flex' : 'hidden'} justify-between items-center mt-2 p-2 w-11/12 rounded-md transition-all duration-150 hover:cursor-pointer hover:bg-neutral-800`}
+      >
+        Friends
+        <UsersRound strokeWidth={1.5} />
+      </MotionLink>
+
+      <motion.form
         action={async () => await signUserOut()}
         className='absolute bottom-2 w-full'
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.5, delay: 0.3 }}
       >
-        <AnimatePresence>
-          <motion.button
-            className={`${openSideBar ? 'flex m-auto' : 'hidden'} items-center justify-between p-2 text-center text-lg w-11/12 rounded-md bg-transparent text-white hover:cursor-pointer hover:bg-neutral-900 transition-all duration-150 mt-2`}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5, delay: 0.5 }}
-          >
-            Sign Out
-            <Power className='ml-5' size={20} />
-          </motion.button>
-        </AnimatePresence>
-      </form>
+        
+        <button
+          className={`${openSideBar ? 'flex m-auto' : 'hidden'} items-center justify-between p-2 text-center text-lg w-11/12 rounded-md bg-transparent text-white hover:cursor-pointer hover:bg-neutral-800 transition-all duration-150 mt-2`}
+        >
+          Sign Out
+          <Power className='ml-5' size={20} />
+        </button>
+      </motion.form>
     </nav>
   );
 }
