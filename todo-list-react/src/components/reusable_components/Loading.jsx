40c7b42af0a998d32a9e50f88af0f25cbc4c604e
gabriel-caesar <gabriel.mdonno@hotmail.com>
2025-08-12 22:53:20 -0400

    fixing issues

--- todo-list-react/src/components/reusable_components/Loading.jsx
+++
@@ -1,6 +0,0 @@
-// this is the loading wheel that can be re-used accross every component
-export default function Loading() {
-  return (
-    <div id='loading'></div> // all the styling attached to this id
-  )
-}
