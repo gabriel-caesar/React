8e3fd72e92cc22f19789fb5f8f1b444ceb927498
gabriel-caesar <gabriel.mdonno@hotmail.com>
2025-11-04 14:58:34 -0500

    Fixed player with 1hp

--- mtg-simplified/src/reducers/player-reducer.js
+++ mtg-simplified/src/reducers/player-reducer.js
@@ -9,7 +9,7 @@ export default function playerReducer(state, action) {
         deck_current_cards: 21,
         hands: [],
         mana_bar: [],
-        hp: 1,
+        hp: 20,
         battlefield: [],
         graveyard: [],
       };
